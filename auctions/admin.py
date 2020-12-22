1e2f7d7e19c0ba9202fca15e33a522a91f86f294
Tutanabg <66039997+Tutanabg@users.noreply.github.com>
2020-12-22 10:28:32 -0500

    Update admin.py

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,1 +1,11 @@
+from django.contrib import admin
+from .models import User, list, bid, comments, watchlist
+# Register your models here.
+admin.site.register(User) 
+admin.site.register(list) 
+admin.site.register(bid)
+admin.site.register(comments)
+admin.site.register(watchlist)
+
+     
 
